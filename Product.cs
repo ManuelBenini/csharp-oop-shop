d5b69e7389378a22c646da019296e0be03e1e675
Manuel Benini <manuelbenini1905@gmail.com>
2022-09-19 16:50:17 +0200

    Create due istanze della classe per testare i due constructor

---
+++ Product.cs
@@ -0,0 +1,92 @@
+﻿using System.Drawing;
+
+public class Product
+{
+
+    // Attributes
+    int code;
+    string name;
+    string description;
+    int price;
+    int iva;
+
+    // costruttore
+    public Product()
+    {
+       code = GenerateCode();
+    }
+
+    // costruttore con parametri
+    public Product(string name, string description, int price, int iva)
+    {
+        code = GenerateCode();
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = iva;
+    }
+
+    // Getters
+    public int GetCode()
+    {
+        return code;
+    }
+    public string GetName()
+    {
+       return name;
+    }
+    public string GetDescription()
+    {
+        return description;
+    }
+    public int GetPrice()
+    {
+        return price;
+    }
+    public int GetIva()
+    {
+        return iva;
+    }
+    public float GetTaxedPrice()
+    {
+        float taxedPrice = ((float)price / 100) * iva;
+        return taxedPrice;
+    }
+
+    public string GetFullName()
+    {
+        string fullname = GetName() + code;
+        return fullname;
+    }
+
+    public string GetFullCode()
+    {
+        char pad = '0';
+        return code.ToString().PadLeft(8, pad);
+    }
+
+    // Setters
+    public void ChangeName(string newName)
+    {
+        name = newName;
+    }
+    public void ChangeDescription(string newDescription)
+    {
+        description = newDescription;
+    }
+    public void ChangePrice(int newPrice)
+    {
+        price = newPrice;
+    }
+    public void ChangeIva(int newIva)
+    {
+        iva = newIva;
+    }
+
+    // Methods
+    private int GenerateCode()
+    {
+        return new Random().Next(1, 999999999);
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Product firstBook = new Product();
+
+firstBook.ChangeName("Do Androids Dream About Electric Sheeps?");
+firstBook.ChangeDescription("Il firstBook che ha ispirato Blade Runner!");
+firstBook.ChangePrice(50);
+firstBook.ChangeIva(22);
+
+
+Console.WriteLine("Nome del primo firstBook: " + firstBook.GetName());
+Console.WriteLine("Descrizione: " + firstBook.GetDescription());
+Console.WriteLine("Prezzo: " + firstBook.GetPrice() + "€");
+Console.WriteLine("Iva: " + firstBook.GetIva() + "%");
+Console.WriteLine("Prezzo tassato: " + firstBook.GetTaxedPrice());
+Console.WriteLine("Codice: " + firstBook.GetCode());
+Console.WriteLine("Nome con codice: " + firstBook.GetFullName());
+Console.WriteLine("Codice completo: " + firstBook.GetFullCode());
+
+Console.WriteLine();
+int terminalWidth = Console.WindowWidth;
+Console.WriteLine(("").PadRight(terminalWidth, '-'));
+Console.WriteLine(("").PadRight(terminalWidth, '-'));
+Console.WriteLine();
+
+Product secondBook = new Product("Io, robot", "Il libro che ha venduto milioni di copie", 150, 22);
+
+Console.WriteLine("Nome del secondBook: " + secondBook.GetName());
+Console.WriteLine("Descrizione: " + secondBook.GetDescription());
+Console.WriteLine("Prezzo: " + secondBook.GetPrice() + "€");
+Console.WriteLine("Iva: " + secondBook.GetIva() + "%");
+Console.WriteLine("Prezzo tassato: " + secondBook.GetTaxedPrice());
+Console.WriteLine("Codice: " + secondBook.GetCode());
+Console.WriteLine("Nome con codice: " + secondBook.GetFullName());
+Console.WriteLine("Codice completo: " + secondBook.GetFullCode());
