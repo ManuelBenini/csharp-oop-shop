e405e37f6f1cc35aeb0562083a228b0d65891874
Manuel Benini <manuelbenini1905@gmail.com>
2022-09-19 17:02:42 +0200

    Fix euro sign in terminal

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+System.Console.Out.WriteLine("œil");
+
+
 Product firstBook = new Product();
 
 firstBook.ChangeName("Do Androids Dream About Electric Sheeps?");
-firstBook.ChangeDescription("Il firstBook che ha ispirato Blade Runner!");
+firstBook.ChangeDescription("Il libro che ha ispirato Blade Runner!");
 firstBook.ChangePrice(50);
 firstBook.ChangeIva(22);
 
 
-Console.WriteLine("Nome del primo firstBook: " + firstBook.GetName());
+Console.WriteLine("Nome del primo libro: " + firstBook.GetName());
 Console.WriteLine("Descrizione: " + firstBook.GetDescription());
 Console.WriteLine("Prezzo: " + firstBook.GetPrice() + "€");
 Console.WriteLine("Iva: " + firstBook.GetIva() + "%");
@@ -16,15 +20,19 @@ Console.WriteLine("Codice: " + firstBook.GetCode());
 Console.WriteLine("Nome con codice: " + firstBook.GetFullName());
 Console.WriteLine("Codice completo: " + firstBook.GetFullCode());
 
-Console.WriteLine();
+
 int terminalWidth = Console.WindowWidth;
+int terminalheight = Console.WindowHeight;
+
+Console.WriteLine();
 Console.WriteLine(("").PadRight(terminalWidth, '-'));
 Console.WriteLine(("").PadRight(terminalWidth, '-'));
 Console.WriteLine();
+
 
 Product secondBook = new Product("Io, robot", "Il libro che ha venduto milioni di copie", 150, 22);
 
-Console.WriteLine("Nome del secondBook: " + secondBook.GetName());
+Console.WriteLine("Nome del secondo libro: " + secondBook.GetName());
 Console.WriteLine("Descrizione: " + secondBook.GetDescription());
 Console.WriteLine("Prezzo: " + secondBook.GetPrice() + "€");
 Console.WriteLine("Iva: " + secondBook.GetIva() + "%");
